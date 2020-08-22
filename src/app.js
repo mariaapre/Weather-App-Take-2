b6d9d80804f3349aabf378dbefad8fc2c6ebc7da
mariaapre <maria.e.apreza@gmail.com>
2020-08-22 17:43:07 -0500

    Deletes "city" from JS

--- src/app.js
+++ src/app.js
@@ -45,7 +45,6 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "bd8ecef3a5464b3533486b9092216486";
-  let city = "Houston";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }

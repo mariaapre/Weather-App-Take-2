3c2dec0c8716486da87a5bb58ce5acede92237d9
mariaapre <maria.e.apreza@gmail.com>
2020-08-24 18:39:10 -0500

    fixed the bug caused by the template string

--- src/app.js
+++ src/app.js
@@ -60,6 +60,7 @@ function displayTemperature(response) {
 
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
   let forecast = null;
 
   for (let index = 0; index < 6; index++) {
